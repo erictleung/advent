12c20f8b0fed24f88e91baa50f56d5a88e707662
Eric Leung <eric@erictleung.com>
2017-12-01 12:10:17 -0800

    Add minimal setup.py file for solutions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='advent2017',
+      version='0.1',
+      description='My solutions to Advent of Code 2017',
+      url='http://github.com/erictleung/advent2017',
+      author='Eric Leung',
+      author_email='eric@erictleung.com',
+      license='MIT',
+      packages=['advent2017'],
+      zip_safe=False)
