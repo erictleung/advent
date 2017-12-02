925ab44753758695965f6843322876823d41103f
Eric Leung <eric@erictleung.com>
2017-12-02 12:03:06 -0800

    Complete second checksum challenge

--- advent2017/corrupt_checksum.py
+++ advent2017/corrupt_checksum.py
@@ -13,6 +13,30 @@ def calc_checksum(in_arr):
     return sum([int(max(arr)) - int(min(arr)) for arr in int_arr])
 
 
+def calc_even_checksum(in_arr):
+    int_arr = two_d_arr_to_num(in_arr)  # Make sure elements are integers
+    final_sum = 0
+    for inner_arr in int_arr:
+        inner_arr.sort()  # Heuristic sort to lessen number of comparisons
+        for idx, val in enumerate(inner_arr):
+            # Divide inner array by an element in array
+            div_array = list(map(lambda x: x % val, inner_arr))
+
+            # Keep indices where remainder from division equal to zero
+            idx_array = [idx for idx, val in enumerate(div_array) if val == 0]
+
+            # Check if there were two numers that were evenly divisible
+            if len(idx_array) == 2:
+                # Access good numbers by indices we've deemed good
+                good_nums = [inner_arr[i] for i in idx_array]
+                break  # Leave for loop, we have numbers we want
+
+        # There's only two numbers so just looking at maxes and mins is okay
+        final_sum += max(good_nums) / min(good_nums)
+
+    return final_sum
+
+
 if __name__ == "__main__":
     # Take file argument input
     data = sys.argv[1]
@@ -21,4 +45,5 @@ if __name__ == "__main__":
     raw = list(csv.reader(open(data, "r"), delimiter="\t"))
 
     # Return calculated checksum
-    print(calc_checksum(raw))
+    print("Checksum:", calc_checksum(raw))
+    print("Even Checksum:", calc_even_checksum(raw))
