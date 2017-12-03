41e8cebb71e8f5613df5595e343ead2ef9162a9b
Eric Leung <eric@erictleung.com>
2017-12-03 14:02:24 -0800

    Add odd number indexing function
    In my brainstorming session, it appears that knowing the index of the
    odd number in the sequence of all odd numbers will be important in
    calculating the step size between a given number and the center.

--- advent2017/spiral_mem.py
+++ advent2017/spiral_mem.py
@@ -4,6 +4,43 @@ import sys
 import csv
 
 
+def odd_number_index(odd):
+    """
+    Return numbered odd number given after one
+
+    Works only on positive numbers.
+
+    :param odd: number to which to check which odd number it is
+    :returns: integer of what odd number is given, -1 if even
+
+    >>> odd_number_index(1)
+    0
+
+    >>> odd_number_index(3)
+    1
+
+    >>> odd_number_index(9)
+    4
+
+    >>> odd_number_index(10)
+    -1
+    """
+    idx = -1
+
+    # If number isn't even odd, don't bother with rest of function
+    if (odd % 2) == 0:
+        return idx
+
+    # Brute force look up what number the odd number is
+    for i in range(odd):
+        odd_num = 2*i + 1
+        if odd_num == odd:
+            idx = i
+            break
+
+    return idx
+
+
 def spiral_mem(number):
     return 0
 
