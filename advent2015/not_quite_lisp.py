e3de7871c52790aef8619766b9643b90cd2361a1
Eric Leung <eric@erictleung.com>
2018-11-27 03:50:07 -0800

    Finish second part of Day 1 in 2015

--- advent2015/not_quite_lisp.py
+++ advent2015/not_quite_lisp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from itertools import accumulate
 
 
 def which_santas_floor(instructions):
@@ -15,8 +16,16 @@ def left_or_right_val(paren):
         return -1
 
 
+def first_char_to_basement(instructions):
+    instruction_translation = list(map(left_or_right_val, instructions))
+    accumulate_sum = accumulate(instruction_translation)
+    neg_one_indices = [idx for idx, x in enumerate(accumulate_sum) if x == -1]
+    return neg_one_indices[0] + 1
+
+
 if __name__ == "__main__":
     data = sys.argv[1]  # Take file argument input
     with open(data, "r") as fh:
         raw = fh.readline().rstrip()
-    print(which_santas_floor(raw))
+    print("Final floor: ", str(which_santas_floor(raw)))
+    print("First basement: ", str(first_char_to_basement(raw)))
--- test/test_santafloor.py
+++ test/test_santafloor.py
@@ -1,5 +1,5 @@
 import unittest
-from advent2015 import which_santas_floor
+from advent2015 import which_santas_floor, first_char_to_basement
 
 
 class TestSantaFloor(unittest.TestCase):
@@ -30,6 +30,14 @@ class TestSantaFloor(unittest.TestCase):
 
     def test_long_negs_two(self):
         self.assertEqual(which_santas_floor(")())())"), -3)
+
+class TestSantaBasement(unittest.TestCase):
+
+    def test_basement_one(self):
+        self.assertEqual(first_char_to_basement(")"), 1)
+
+    def test_basement_two(self):
+        self.assertEqual(first_char_to_basement("()())"), 5)
 
 if __name__ == '__main__':
     unittest.main()
