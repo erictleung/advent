23af86de70b323e55813af9ca4f7736dd5dd45fc
Eric Leung <eric@erictleung.com>
2018-11-27 03:19:12 -0800

    Finish first part of Day 1 in 2015

---
+++ advent2015/__init__.py
@@ -0,0 +1,1 @@
+from .not_quite_lisp import *
--- advent2015/not_quite_lisp.py
+++ advent2015/not_quite_lisp.py
@@ -1,2 +1,22 @@
-def which_santas_floor():
-    return 0
+#!/usr/bin/env python
+
+import sys
+
+
+def which_santas_floor(instructions):
+    instruction_translation = map(left_or_right_val, instructions)
+    return sum(instruction_translation)
+
+
+def left_or_right_val(paren):
+    if paren == "(":
+        return 1
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    data = sys.argv[1]  # Take file argument input
+    with open(data, "r") as fh:
+        raw = fh.readline().rstrip()
+    print(which_santas_floor(raw))
